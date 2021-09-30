48e6767c2d4fd638ca7000df0d59f6034b6650cd
alexdack <avokoala@gmail.com>
2021-09-30 14:48:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Khalil! Commit!
+          Yo KhaliL! This was deployed in one click!
         </p>
         <a
           className="App-link"
